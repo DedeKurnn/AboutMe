64172500dca99009eb6f3b537478541ea35007d1
Dede Kurniawan <dikeplayz.official@gmail.com>
2022-02-17 11:08:23 +0700

    About my Self App update

--- app/src/main/java/com/dedekurnn/aboutdede/MainActivity.kt
+++ app/src/main/java/com/dedekurnn/aboutdede/MainActivity.kt
@@ -14,31 +14,12 @@ import com.dedekurnn.aboutdede.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val myNameDede: MyName = MyName("Dede Kurniawan")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.myName = myNameDede
-        binding.doneButton.setOnClickListener { addNickName((it)) }
 
     }
 
-    private fun addNickName(view: View) {
 
-        binding.apply {
-            myName?.name = nicknameEditText.text.toString()
-            nicknameText.text = nicknameEditText.text
-            nicknameText.visibility = View.VISIBLE
-            nicknameEditText.visibility = View.GONE
-            doneButton.visibility = View.GONE
-            invalidateAll()
-        }
-
-
-        // Hide the keyboard
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
 }
